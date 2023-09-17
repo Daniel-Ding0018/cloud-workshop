789d79daf848ad654f9bc9cc9440f1c345cb6c7c
Daniel Ding <ddin0018@student.monash.edu>
2023-09-17 23:44:07 +1000

    ohtis

--- src/components/addphoto.js
+++ src/components/addphoto.js
@@ -1,10 +1,42 @@
 import Button from "react-bootstrap/Button";
+import { Form } from "react-bootstrap"
 import React from "react";
+import {PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
+import { useRef } from "react";
 
 function AddPhoto() {
+  // Image ref
+  const imageRef = useRef(null);
+
+  // Amazon credentials
+
+  const CREDENTIAL = {
+    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+  };
+
+  const REGION = "ap-southeast-2";
+
+  const client = new S3Client({ region: REGION, credentials: CREDENTIAL });
+
+
+  const uploadPhoto = async () => {
+    const inputImage = imageRef.current;
+    console.log(inputImage)
+
+  }
+
+
   return (
     <>
-      <Button variant="outline-primary">Primary</Button>{" "}
+    <Form>
+      <Form.Group controlId="formFile" className="mb-3">
+        <Form.Control type="file" ref={imageRef}/>
+      </Form.Group>
+      <Button variant="primary" onClick={uploadPhoto}>
+        Submit
+      </Button>
+    </Form>
     </>
   );
 }
