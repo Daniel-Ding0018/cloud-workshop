bbf4176acbd07a902749c58dd3a3758503647228
Daniel Ding <ddin0018@student.monash.edu>
2023-09-17 17:45:53 +1000

    prettier

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,88 +1,94 @@
-import { Container, Image, Stack } from "react-bootstrap";
-import React, { useState } from "react";
+import { Container,Image, Stack } from "react-bootstrap"
+import React, { useState } from 'react'
 import { useEffect } from "react";
-import {
-  GetObjectCommand,
-  S3Client,
-  ListObjectsCommand,
-} from "@aws-sdk/client-s3";
+import { GetObjectCommand, S3Client, ListObjectsCommand } from "@aws-sdk/client-s3";
 
-export default function Home() {
-  const [image, setImage] = useState(<p>Loading...</p>);
+export default function Home(){
 
-  const CREDENTIAL = {
-    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
-    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
-  };
+    const [image, setImage] = useState()
 
-  const REGION = "ap-southeast-2";
+    const CREDENTIAL = {
+        accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
+        secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
+      };
 
-  const client = new S3Client({ region: REGION, credentials: CREDENTIAL });
+    const REGION = "ap-southeast-2"
 
-  // List objects in the bucket
-  const getListObjects = async () => {
-    const command = new ListObjectsCommand({
-      Bucket: "image-bucket-mac",
-    });
+    const client = new S3Client({region:REGION,credentials:CREDENTIAL})
 
-    try {
-      const { Contents } = await client.send(command);
+      // List objects in the bucket
+    const getListObjects = async () => {
+      const command = new ListObjectsCommand({
+        Bucket: "image-bucket-mac",
+        });
+    
+      try {      
+        const { Contents } = await client.send(command);
 
-      return Contents.filter(
-        (c) => c.Key.substring(c.Key.length - 3) === "jpg",
-      );
-    } catch (err) {
-      console.error(err);
+        return Contents.filter((c) => c.Key.substring(c.Key.length - 3) === "jpg");
+      } catch (err) {
+        console.error(err);
+      }
     }
-  };
 
-  // Get object in bucket
-  const getObject = async (bucketKey) => {
-    const command = new GetObjectCommand({
-      Bucket: "image-bucket-mac",
-      Key: bucketKey.Key,
-    });
 
-    try {
-      const response = await client.send(command);
-      // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods
-      const uintarray = (await response.Body.transformToByteArray()).buffer;
-      const imageblob = URL.createObjectURL(
-        new Blob([uintarray], { type: "image/png" } /* (1) */),
-      );
-      return imageblob;
-      // imageReturn.append("hi");
-      // setImage(<></>)
-    } catch (err) {
-      // console.error(err);
+    // Get object in bucket
+    const getObject = async (bucketKey) =>{
+      const command = new GetObjectCommand({
+        Bucket: "image-bucket-mac",
+        Key: bucketKey.Key
+      });
+
+      try {
+        const response = await client.send(command);
+        // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods
+        const uintarray = (await response.Body.transformToByteArray()).buffer
+        const imageblob =  URL.createObjectURL(
+          new Blob([uintarray], { type: 'image/png' } /* (1) */)
+        );
+        return imageblob
+        // imageReturn.append("hi");    
+        // setImage(<></>)
+
+      } catch (err) {
+        // console.error(err);
     }
-  };
-
-  // GetObject
-  const mainGet = async (bucketContents) => {
-    // Loop thru each object that we want to display
-    let bucketBlobs = [];
-    bucketContents.forEach((bucketKey) => {
-      //   setImage(await getObject(bucketKey))
-      bucketBlobs.push(getObject(bucketKey));
-    });
-
-    bucketBlobs = await Promise.all(bucketBlobs);
-
-    return bucketBlobs;
-  };
-
-  useEffect(() => {
-    async function fetchData() {
-      // const bucketKeys = await getListObjects();
-      // let bucketImages = await mainGet(bucketKeys)
-      // setImage(bucketImages.map((bucketImage, index) => <Image src={bucketImage} width={200} height={200}></Image>))
     }
-    fetchData();
-  }, []);
 
-  return (
-    <>
+          // GetObject
+      const  mainGet = async (bucketContents) => {
+        // Loop thru each object that we want to display
+        let bucketBlobs = [];
+        bucketContents.forEach((bucketKey) =>{
+        //   setImage(await getObject(bucketKey))
+          bucketBlobs.push(getObject(bucketKey))
+        }) 
+
+        bucketBlobs = await Promise.all(bucketBlobs)
+
+        return bucketBlobs
+
+        
+        // Make the JSX
+        // console.log(imageBlobHolder)
+        // setImage(      <div className="p-2"><Image src={imageBlobHolder[0]}></Image></div>        )
+
+        };
+
+
+    useEffect(()=>{
+      async function fetchData(){
+        const bucketKeys = await getListObjects();
+        let bucketImages = await mainGet(bucketKeys)
+        setImage(bucketImages.map(bucketImage => <Image src={bucketImage} width={200} height={200}></Image>))
+      }fetchData()
+    }, [])
+
+
+
+
+    return (
+      <>
       <style type="text/css">
         {`
         .Image {
@@ -91,10 +97,14 @@ export default function Home() {
         }
         `}
       </style>
-      <Container>
-        <h1>Your Photos</h1>
-        {image}
-      </Container>
-    </>
-  );
+        <Container>
+            <h1>Your Photos</h1>
+            <Stack gap={3} direction="horizontal">
+              {image}
+            </Stack>
+        </Container>
+      </>
+        
+    )
 }
+
