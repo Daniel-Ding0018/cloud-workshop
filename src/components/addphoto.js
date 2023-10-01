14b4c7c57b1674adf5697e415424a0199a71f653
Daniel Ding <ddin0018@student.monash.edu>
2023-10-02 01:28:51 +1100

    toasts are finished

--- src/components/addphoto.js
+++ src/components/addphoto.js
@@ -1,12 +1,15 @@
 import Button from "react-bootstrap/Button";
-import { Form } from "react-bootstrap"
+import { Toast, ToastContainer, Form } from "react-bootstrap"
 import React from "react";
 import {PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
 import { useState } from "react";
 
+let toastCounter = 0
+
 function AddPhoto() {
   // Image ref
   const [file, setFile] = useState();
+  const [toasts, setToasts] = useState([]);
 
   // Amazon credentials
 
@@ -35,11 +38,16 @@ function AddPhoto() {
 
     try {
       const response = await client.send(command);
+      setToasts([
+        ...toasts,
+        {id: toastCounter++, name: file.name}
+      ])
       console.log(response);
     } catch (err) {
       console.error(err);
     }
   }
+
 
   return (
     <>
@@ -51,6 +59,33 @@ function AddPhoto() {
         Submit
       </Button>
     </Form>
+    <ToastContainer className="p-3"
+          position="bottom-end"
+          style={{ zIndex: 1 }}>
+      {/* <Toast show={true} id="1">
+        <Toast.Header>
+          <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+          <strong className="me-auto">Bootstrap</strong>
+          <small className="text-muted">just now</small>
+        </Toast.Header>
+        <Toast.Body>See? Just like this.</Toast.Body>
+      </Toast>
+      <Toast show={true} id="2">
+        <Toast.Header>
+          <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+          <strong className="me-auto">Bootstrap</strong>
+          <small className="text-muted">2 seconds ago</small>
+        </Toast.Header>
+        <Toast.Body>Heads up, toasts will stack automatically</Toast.Body>
+      </Toast> */}
+      {toasts.map(toast => <Toast show={true} onClose={() => setToasts(toasts.filter(t => t.id !== toast.id))}>
+        <Toast.Header>
+          <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+          <strong className="me-auto">Photo Uploaded</strong>
+        </Toast.Header>
+        <Toast.Body>{toast.name} was successfully uploaded</Toast.Body>
+      </Toast>)}
+    </ToastContainer>
     </>
   );
 }
